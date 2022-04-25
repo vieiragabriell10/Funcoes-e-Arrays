e2a8219cf6e456546d7770ebd017e812d1e2ef33
Gabriel Vieira <devgabrielv@gmail.com>
2022-04-25 08:59:03 -0300

    Funcoes-e-Arrays

---
+++ index.js
@@ -0,0 +1,10 @@
+function positions( firstPlace, secondPlace, lastPlace){
+    return `fistPlace`
+}
+let nomeDosCompetidores = ["Pedro", "Gabriel", "Daniel"]
+let posicoesDoPodio = [1,2,3]
+
+console.log(nomeDosCompetidores)
+console.log(posicoesDoPodio[0] = "1ª Colocado: Daniel")
+console.log(posicoesDoPodio[1] = "2ª Colocado: Pedro")
+console.log(posicoesDoPodio[2] = "3ª Colocado: Gabriel")
